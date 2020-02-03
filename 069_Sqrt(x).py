c4a79242d27b7cede92b68377191dafc8ecaf774
OleksaDub <alex.dubrovsky@yahoo.com>
2020-02-03 22:08:34 +0200

    069

---
+++ 069_Sqrt(x).py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+69. Sqrt(x)
+Created on Mon Feb  3 21:58:11 2020
+@author: oleksadub
+"""
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        import math
+        return int(math.sqrt(x))
+        
+
+'''
+Runtime: 24 ms, faster than 97.21% of Python3 online submissions 
+for Sqrt(x).
+Memory Usage: 12.6 MB, less than 100.00% of Python3 online submissions 
+for Sqrt(x)
+'''
