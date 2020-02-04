d7a4093e98b015eb1bfe55577eab36add72785cd
OleksaDub <alex.dubrovsky@yahoo.com>
2020-02-04 23:18:22 +0200

    050

---
+++ 050_Pow(x,n).py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+50. Pow(x, n)
+Created on Tue Feb  4 23:12:11 2020
+@author: oleksadub
+"""
+class Solution:
+    def myPow(self, x: float, n: int) -> float:
+        return x**n
+        
+
+'''
+Runtime: 24 ms, faster than 88.06% of Python3 online submissions 
+for Pow(x, n).
+Memory Usage: 12.7 MB, less than 100.00% of Python3 online submissions 
+for Pow(x, n).
+'''
